f8760be136b95270e059effd2929c678eac79647
Tuture57 <41503161+Tuture57@users.noreply.github.com>
2020-05-05 20:07:52 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,18 +1,14 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "$";
-// 000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
   console.log("Le bot a bien démarré !!!")
 })
-// 000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 client.on("message", msg => {
   if (msg.content === "ping") {
     msg.reply("pong")
   }
-// 000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
   client.user.setActivity("ouvrir des caisses", {type: "PLAYING"})
-//0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 })
 client.login("process.env.TOKEN")
