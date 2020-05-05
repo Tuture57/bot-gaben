d5acbac1e6e7c7000026ecad8cec829a6b692104
Tuture57 <41503161+Tuture57@users.noreply.github.com>
2020-05-05 21:42:03 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,17 +1,17 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = "$";
+client.login(process.env.TOKEN);
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-  console.log("Le bot a bien démarré !!!")
-})
+});
+console.log("Le bot a bien démarré !")
 client.on('ready', () => {
     
-    client.user.setActivity("twitch.tv/tuturetv", {type: "WATCHING"})
+  client.user.setActivity("ouvrir des caisses", {type: "PLAYING"})
 
-client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("pong")
-  }
+    client.on("message", msg => {
+      if (msg.content === "ping") {
+        msg.reply("pong")
+      }
+    })
 })
-client.login(process.env.TOKEN)
