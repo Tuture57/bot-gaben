b95b893585473b0d4f35ac6beb65bcd653b0d650
Tuture57 <41503161+Tuture57@users.noreply.github.com>
2020-05-05 21:24:10 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,7 +5,10 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
   console.log("Le bot a bien démarré !!!")
 })
-client.user.setActivity("twitch.tv/tuturetv", {type: "WATCHING"})
+client.on('ready', () => {
+    
+    client.user.setActivity("twitch.tv/tuturetv", {type: "WATCHING"})
+
 client.on("message", msg => {
   if (msg.content === "ping") {
     msg.reply("pong")
