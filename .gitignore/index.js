996ad003b42c8cb22f6bd8e050f3f097252fe82d
Tuture57 <41503161+Tuture57@users.noreply.github.com>
2020-05-05 14:11:27 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,14 +8,5 @@ console.log("SA MARCHE !!!")
 client.on('ready', () => {
     
     client.user.setActivity("ouvrir des caisses", {type: "PLAYING"})
-
-var bot = require("discord-music-bot");
- 
-var serverName = "BOT TEST";
-var textChannelName = "général";
-var voiceChannelName = "test";
-var aliasesFile = "bot-gaben";
-var botToken = (process.env.TOKEN);
- 
-bot.run(serverName, textChannelName, voiceChannelName, aliasesFile, botToken);
+    
 })
