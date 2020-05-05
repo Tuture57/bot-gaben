4c4f86ec8e4f106417b3d6dc613422086531c772
Tuture57 <41503161+Tuture57@users.noreply.github.com>
2020-05-05 11:31:17 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "$";
+client.login(process.env.TOKEN);
+client.on("ready", () => {
+});
+console.log("SA MARCHE !!!")
+client.on('ready', () => {
+    
+    client.user.setActivity("ouvrir des caisses", {type: "PLAYING"})
+
+})
