126689637885f77d8e3a94aadba14038ddf35f7b
Tuture57 <41503161+Tuture57@users.noreply.github.com>
2020-05-05 18:17:07 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,12 +1,34 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-var prefix = "$";
-client.login(process.env.TOKEN);
-client.on("ready", () => {
+const { Client } = require("discord.js");
+const { config } = require("dotenv");
+
+// Declares our bot,
+// the disableEveryone prevents the client to ping @everyone
+const client = new Client({
+    disableEveryone: true
 });
-console.log("SA MARCHE !!!")
-client.on('ready', () => {
-    
-    client.user.setActivity("ouvrir des caisses", {type: "PLAYING"})
-    
+
+config({
+    path: __dirname + "/.env"
 })
+
+// When the bot's online, what's in these brackets will be executed
+client.on("ready", () => {
+    console.log(`Hi, ${client.user.username} is now online!`);
+
+    // Set the user presence
+    client.user.setPresence({
+        status: "online",
+        game: {
+            name: "me getting developed",
+            type: "WATCHING"
+        }
+    }); 
+})
+
+// When a message comes in, what's in these brackets will be executed
+client.on("message", async message => {
+    console.log(`${message.author.username} said: ${message.content}`);
+});
+
+// Login the bot
+client.login(process.env.TOKEN);
